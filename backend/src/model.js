fc9b33e19f0075d436695dbd093bb0a9ec398aea
davidvergara03 <dvergaraagudelo2473@conestogac.on.ca>
2024-03-11 11:11:30 -0400

    Spring 2

---
+++ backend/src/model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const formSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+});
+
+const Form = mongoose.model('Form', formSchema, 'form');
+
+module.exports = Form;
